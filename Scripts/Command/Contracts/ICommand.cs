83e6d7fc63445b2ca115282a4a3f042fd0746180
Kyappy <m.gavanier@gmail.com>
2017-10-18 00:31:58 +0100

    Added command utility interface

---
+++ Scripts/Command/Contracts/ICommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Utility.Command.Contracts {
+	/// <summary>
+	/// Command interface.
+	/// </summary>
+	/// <typeparam name="T">Type of the command target.</typeparam>
+	public interface ICommand<in T> {
+		/// <summary>
+		/// Executes the command on the specified target.
+		/// </summary>
+		/// <param name="target">The target on which to execute the command.</param>
+		void Execute(T target);
+	}
+}
