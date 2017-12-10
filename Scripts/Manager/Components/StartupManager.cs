37a3773949d011583219d780cb3605ead1074cef
Kyappy <m.gavanier@gmail.com>
2017-12-10 10:27:49 +0000

    Manager control scripts added

---
+++ Scripts/Manager/Components/StartupManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UtilityModule.Load.Components;
+using UtilityModule.Manager.Contracts;
+
+namespace UtilityModule.Manager.Components {
+	/// <summary>
+	/// Startup manager component.
+	/// </summary>
+	[RequireComponent(typeof(DontDestroyOnLoad))]
+	public class StartupManager : MonoBehaviour {
+		#region private fields
+		/// <summary>
+		/// The children managers.
+		/// </summary>
+		private ManagerBase[] managers;
+		#endregion
+
+		#region unity methods
+		/// <summary>
+		/// Awakes this instance.
+		/// </summary>
+		private void Awake() {
+			managers = GetComponentsInChildren<ManagerBase>();
+		}
+		 
+		private IEnumerator Start() {
+			var allManagersReady = false;
+			while (!allManagersReady) {
+				allManagersReady = managers.Aggregate(true, (current, manager) => current & manager.Ready);
+				yield return null;
+			}
+
+			// todo: throw managers ready event
+		}
+		#endregion
+	}
+}
---
+++ Scripts/Manager/Contracts/ManagerBase.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UtilityModule.Manager.Contracts {
+	/// <summary>
+	/// Manager base abstract class.
+	/// </summary>
+	public abstract class ManagerBase: MonoBehaviour {
+		#region public accessors
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="ManagerBase"/> is ready.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if ready; otherwise, <c>false</c>.
+		/// </value>
+		public bool Ready { get; protected set; }
+		#endregion
+	}
+}
