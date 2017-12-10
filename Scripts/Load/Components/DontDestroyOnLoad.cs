36188b68fa75415bc57189525a0b63cf9409d450
Kyappy <m.gavanier@gmail.com>
2017-12-10 10:26:25 +0000

    Dont destroy on load script added

---
+++ Scripts/Load/Components/DontDestroyOnLoad.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UtilityModule.Load.Components {
+	/// <summary>
+	/// Don't destroy on load component.
+	/// </summary>
+	public class DontDestroyOnLoad : MonoBehaviour {
+		#region unity methods
+		/// <summary>
+		/// Awakes this instance.
+		/// </summary>
+		private void Awake() {
+			DontDestroyOnLoad(gameObject);
+		}
+		#endregion
+
+	}
+}
